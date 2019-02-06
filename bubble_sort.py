3a6c0ff30b6b59af09d3207c5f3e376ee16dcb5e
cneally <42223729+cneally@users.noreply.github.com>
2019-02-06 22:35:08 +0000

    Addition of comments to explain code

--- bubble_sort.py
+++ bubble_sort.py
@@ -13,7 +13,16 @@ def bubble_sort_algorithm(array):
     # Loop through the entire array
     for i in range(n):
 
+       # Here we repeatedly step through the list comparing adjacent pairs
+         # If an element is larger than the element to the right of it will get swapped
+         # The array is traversed until all elements have been sorted
+         
         for j in range(0, n-i-1):
+        
+         # EXAMPLE:
+         # arr = [1, 5, 2, 6, 4]
+         # first iteration => [1,2,5,4,6]
+         # Second iteration = [1,2,4,5,6]
             
             if array[j] > array[j+1]:
                 array[j], array[j+1] = array[j+1], array[j]
